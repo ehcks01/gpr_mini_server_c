91c41d248d54c1b560fa5823da3db5f8f868b9e2
ehcks01 <dochanop@gmail.com>
2021-07-05 02:54:36 +0900

    혹시나 몰라서 삭제 추가..

--- host_restart.sh
+++ host_restart.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-path=$(dirname $(realpath $0))
+#arr_stop_target_list=$(ps -ef |grep "gpr_mini" |grep -v "grep" |awk '{print $2}')
+#for target_stop in ${arr_stop_target_list};do
+#        kill -9 $target_stop
+#        echo kill process $target_stop
+#done
 
+path=$(dirname $(realpath $0))
 sudo service hostapd restart
 echo hospad restart
 sleep 5
